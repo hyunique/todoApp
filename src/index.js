79efdff275f340acdf8674725619d5828875a687
Elena <kunstvanelena@gmail.com>
2023-03-14 16:56:24 +0100

    implemented class and functions for project data

--- src/index.js
+++ src/index.js
@@ -1,111 +1,85 @@
-// Create new todos
+const gridContainer = document.querySelector('.grid-container')
+const projectInput = document.querySelector('.project--input')
+const projectList = document.querySelector('.project--list')
+const projectItem = document.querySelector('.project--item')
+const taskOverview = document.querySelector('.overview')
+const taskInput = document.querySelector('.task--input')
+const taskList = document.querySelector('.task--list')
+const taskItem = document.querySelector('.task--item')
+const checkbox = document.querySelector('.checkbox')
+const icons = document.querySelector('.material-symbols-outlined')
 
-let projects=[]
+const projects = []
 
-
-
-class Project {
-    constructor(name,taskList) {
+class Project{
+    constructor(name, task, id= `P${(Date.now() + '').slice(-6)}`){
         this.name = name;
-        this.taskList = {taskList}
+        this.task = {task}
+        this.id = id;
     }
+
+    // pushProject(obj) {
+    //     projects.push(obj)
+    // }
 }
 
-class Task {
-    
-    constructor(title, note, due, priority, id = (Date.now() + '').slice(-6)) {
+class Task extends Project{
+    constructor(title, id = `T${(Date.now() + '').slice(-6)}`) {
         this.title = title;
-        this.note = note;
-        this.due = due;
-        this.priority = priority;
-        this.id = id
-    }
-
-    addToProject(task){
-        projects.push(task)
-    }
-
-    removeTask() {
-        projects.find(x => x === this)
-    }
-
-    set due(dueDate) {
-        this.due = dueDate
-    }
-
-    get due() {
-        return this.due
-    }
-
-}
-
-
-// DOM Manipulation
-
-const form = document.querySelector('.form')
-const title = document.querySelector('.title')
-const description = document.querySelector('.description')
-const dueDate = document.querySelector('.duedate')
-const priority = document.querySelector('.priority')
-const addBtn = document.querySelector('.addTask')
-const projectContainer = document.querySelector('.project')
-const pjtTitle = document.querySelector('.project--title')
-const pjtBtn = document.querySelector('.addProject')
-
-class DOM {
-    constructor() {      
-        addBtn.addEventListener('click', this.addTaskData)
-        addBtn.addEventListener('click', this.showTasks)
-        pjtBtn.addEventListener('click', this.addNewProject)
-        pjtBtn.addEventListener('click', this.showProjects)
-    }
-
-    addNewProject(e) {
-        e.preventDefault() 
-        let pjt = new Project(pjtTitle.value)
-        
-    }
-
-    addTaskData(e) {
-        e.preventDefault()   
-        let task = new Task(title.value, description.value, dueDate.value, priority.value)
-        task.addToProject(task)
-
-    }  
-
-    showProjects() {
-        let html = `
-        <h2 class="project--title">${pjtTitle.value}</h3>
-        `
-        projectContainer.insertAdjacentHTML('afterbegin',html)
-
-    }
-
-    showTasks() {
-      
-            let html = `
-            <h3 class="task--title">${title.value}</h3>
-            <h5 class="task--description">${description.value}</h3>
-            <h5 class="task--due">${dueDate.value}</h3>
-            <h5 class="task--priority">${priority.value}</h3>
-            `
-            const taskCard = document.querySelector('.task--card')
-            taskCard.insertAdjacentHTML('afterbegin', html)
-        
-        //projects.forEach(pjt => {
-        //     let html = `
-        //     <h3 class="task--title">${pjt.title}</h3>
-        //     <h5 class="task--description">${pjt.note}</h3>
-        //     <h5 class="task--due">${pjt.due}</h3>
-        //     <h5 class="task--priority">${pjt.priority}</h3>
-        //     `
-        //     const taskCard = document.querySelector('.task--card')
-        //     taskCard.insertAdjacentHTML('afterbegin', html)
-        // })
-        
+        this.id = id;
     }
 }
 
-const dom = new DOM()
+(function app() {
+    createNewProject('General')
 
-// create project object and include todo task in the project
+
+    projectInput.addEventListener('keydown', getProjectValue)
+    // projectInput.addEventListener('keydown', getValue)
+    taskInput.addEventListener('keydown', getTaskValue)
+    
+})()
+
+function createNewProject(pjtname) {
+    projects.push(new Project(pjtname))
+}
+
+// Get value from input value
+function getProjectValue(e) {
+    if (e.key === 'Enter') {
+        displayProjectInput(this.value)
+        createNewProject(this.value)
+        this.value =''
+    }
+}
+
+// Take input value and render it on screen
+function displayProjectInput(value) {
+    let html = `<li class="project--item">${value.slice(0,1).toUpperCase()+value.slice(1)}</li>`
+    projectList.insertAdjacentHTML('beforeend',html)
+}
+
+function getTaskValue(e) {
+    if (e.key === 'Enter') {
+        displayTaskInput(this.value)
+        this.value =''
+    }
+}
+
+function displayTaskInput(value) {
+    let html = `
+    <li class="task--item">
+    <i class="material-symbols-outlined">drag_indicator</i>
+    <input type="checkbox" class="checkbox">
+    ${value.slice(0, 1).toUpperCase() + value.slice(1)}
+    <div class="icons">
+        <i class="material-symbols-outlined">edit</i>
+        <i class="material-symbols-outlined">delete</i>
+    </div>
+    </li>
+    `
+    taskList.insertAdjacentHTML('beforeend',html)
+}
+
+
+//TODO implement function to connect task with a project
