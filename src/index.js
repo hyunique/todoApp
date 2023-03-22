773a4bcd3253c16c2a2ffbd414a5ad81664234f3
Elena <kunstvanelena@gmail.com>
2023-03-22 13:46:53 +0100

    fixed bug in findProject(), refactored functions

--- src/index.js
+++ src/index.js
@@ -31,8 +31,10 @@ class Task{
 
 (function app() {
     createNewProject('General')
+    displayProjectInput('General')
     projectList.firstElementChild.classList.add('active')
     createNewProject('Holiday')
+    displayProjectInput('Holiday')
    
 
 /* This code below adds a single click event listener to the project list element and uses event delegation to determine which project item was clicked. It then removes the active class from any previously active item and adds it to the clicked item.
@@ -46,7 +48,7 @@ By using event delegation, you reduce the number of event listeners attached to 
           taskList.textContent=''
         }
         event.target.classList.add('active');
-        renderProjectTasks()
+        renderTasks()
         }
     });
 
@@ -55,10 +57,19 @@ By using event delegation, you reduce the number of event listeners attached to 
   
 })()
 
+function getProjectValue(e) {
+    // Create new project and display with input value
+    if (e.key === 'Enter') {
+        createNewProject(this.value)
+        displayProjectInput(this.value)
+        this.value =''
+    }
+}
 function createNewProject(pjtname) {
     // create new project object and push it to array
-    projects.push(new Project(pjtname))
-    displayProjectInput(pjtname)
+    projects.push(new Project(pjtname.slice(0,1).toUpperCase()+pjtname.slice(1)))
+    
+
 }
 
 function displayProjectInput(value) {
@@ -71,45 +82,77 @@ function displayProjectInput(value) {
     // projectList.appendChild(li)
 }
 
-function getProjectValue(e) {
-    // Create new project and display with input value
-    if (e.key === 'Enter') {
-        createNewProject(this.value)
-        this.value =''
-    }
+
+function findProject() {
+    
+    const projectItemAll = document.querySelectorAll('.project--item')
+    // Find a project that contains 'active' class (=clicked by user)
+        const activeProject = [...projectItemAll]
+        .find(project => project.classList.contains('active')).textContent
+
+    // Find a exact data in 'proejcts' array that matches clicked project
+        const activeProjectData = projects
+        .find(project => project.name === activeProject).task
+    // Return an object that contains both the project element and project data
+    return {
+        element: activeProject,
+        data: activeProjectData
+    };
 }
 
-// When user push Enter key on task input, find clicked project and add task to that project
-const projectItemAll = document.querySelectorAll('.project--item')
 function createNewTask(taskname) {
-    projectItemAll.forEach(item => {
-        if (item.classList.contains('active')) {
-            const selectedProject = projects.find(pjt=>pjt.name===item.textContent).task
-            selectedProject.push(new Task(`${taskname.slice(0, 1).toUpperCase() + taskname.slice(1)}`))
-        }
-    })
+    // // Find a project that contains 'active' class (=clicked by user)
+    // const activeProject = [...projectItemAll]
+    //     .find(project => project.classList.contains('active')).textContent
+    // // Find a exact data in 'proejcts' array that matches clicked project
+    // const activeProjectData = projects
+    //     .find(project => project.name === activeProject).task
+
+    // Add new task in the selected project
+    findProject().data.push(new Task(`${taskname.slice(0, 1).toUpperCase() + taskname.slice(1)}`))
+    renderTasks();
+
 }
 
-function renderProjectTasks() {
-    projectItemAll.forEach(item => {
-        if (item.classList.contains('active')) {
-            const selectedProject = projects.find(pjt => pjt.name === item.textContent).task
-            selectedProject.forEach(task => {
-                let html = `
-                    <li class="task--item">
+function renderTasks() {
+
+findProject().data.forEach(task => {
+            let html = `
+                <li class="task--item">
                     <i class="material-symbols-outlined">drag_indicator</i>
                     <input type="checkbox" class="checkbox">${task.title}
                     <div class="icons">
-                    <i class="material-symbols-outlined">edit</i>
-                    <i class="material-symbols-outlined">delete</i>
+                        <i class="material-symbols-outlined">edit</i>
+                        <i class="material-symbols-outlined">delete</i>
                     </div>
-                    </li>
+                </li>
                 `
-                taskList.insertAdjacentHTML('beforeend', html)
-            })
-        }
+            taskList.insertAdjacentHTML('beforeend', html)
     })
 }
+
+
+
+// function renderProjectTasks() {
+//     projectItemAll.forEach(item => {
+//         if (item.classList.contains('active')) {
+//             const selectedProject = projects.find(pjt => pjt.name === item.textContent).task
+//             selectedProject.forEach(task => {
+//                 let html = `
+//                     <li class="task--item">
+//                     <i class="material-symbols-outlined">drag_indicator</i>
+//                     <input type="checkbox" class="checkbox">${task.title}
+//                     <div class="icons">
+//                     <i class="material-symbols-outlined">edit</i>
+//                     <i class="material-symbols-outlined">delete</i>
+//                     </div>
+//                     </li>
+//                 `
+//                 taskList.insertAdjacentHTML('beforeend', html)
+//             })
+//         }
+//     })
+// }
 function displayTaskInput(value) {
     let html = `
     <li class="task--item">
@@ -128,12 +171,11 @@ function displayTaskInput(value) {
 function getTaskValue(e) {
     if (e.key === 'Enter') {
         createNewTask(this.value)
-        
-        displayTaskInput(this.value)
         this.value =''
     }
 }
 
+// const projectItemAll = document.querySelectorAll('.project--item')
 
 
 
